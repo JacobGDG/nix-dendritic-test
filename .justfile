6e5c246524f20320dfab76af9df8fde0d0f26443
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-04-14 18:47:55 +0100

    feat: add home-manager

--- .justfile
+++ .justfile
@@ -1,2 +1,5 @@
 write-flake:
   nix run .#write-flake --show-trace
+
+check:
+  nix flake check --show-trace --all-systems
--- modules/flake/inputs.nix
+++ modules/flake/inputs.nix
@@ -1,14 +1,18 @@
 { inputs, lib, ... }:
 {
-  imports = [
-    inputs.flake-file.flakeModules.default
+  imports = with inputs.flake-file.flakeModules; [
+    default
+    import-tree
   ];
   flake-file = {
     inputs = {
       flake-file.url = lib.mkDefault "github:vic/flake-file";
       flake-parts.url = "github:hercules-ci/flake-parts";
-      import-tree.url = "github:vic/import-tree";
       nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
     outputs = "inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)";
   };
